53f7e134e86e145b425196a64abcc85158fedeee
DRECO <rishabh.private.in@gmail.com>
2025-03-20 23:18:40 +0530

    Create crypto.py

---
+++ src/utilities/crypto.py
@@ -0,0 +1,11 @@
+from cryptography.fernet import Fernet
+
+class Crypto:
+    def __init__(self, key: str):
+        self.cipher = Fernet(key.encode())
+    
+    def encrypt(self, data: str) -> str:
+        return self.cipher.encrypt(data.encode()).decode()
+    
+    def decrypt(self, encrypted_data: str) -> str:
+        return self.cipher.decrypt(encrypted_data.encode()).decode()
